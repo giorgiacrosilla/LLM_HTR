c45da0d373174a4ca5aa5f4818a170e8e0a20390
Giorgia Crosilla <giorgia.crosilla@studio.unibo.it>
2024-09-11 14:30:37 +0200

    Update phi3.py

--- phi3.py
+++ phi3.py
@@ -7,7 +7,7 @@ from transformers import AutoProcessor
 
 model_id = "microsoft/Phi-3-vision-128k-instruct"
 
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cuda", trust_remote_code=True, torch_dtype="auto", _attn_implementation='flash_attention_2') # use _attn_implementation='eager' to disable flash attention
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cuda", trust_remote_code=True, torch_dtype="auto", _attn_implementation='eager') # use _attn_implementation='eager' to disable flash attention
 
 processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
 
